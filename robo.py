67318a6dd77002bd9e68fdc263e41420269f722b
Guilherme A. Santos <270210.gui@gmail.com>
2026-02-01 23:23:54 -0300

    Reformat code in robo.py for consistency

--- robo.py
+++ robo.py
@@ -4,17 +4,18 @@ import pandas as pd
 #Simulação de coleta de dados
 def coletar_dados():
     return [
-        {"data" : datetime.date.today(), "evento": "Processamento Finalizado","Status": "OK"}
+        {"data": datetime.date.today(), "evento": "Processamento Finalizado","Status": "OK"}
     ]
 
 #Salvar em um CSV
 def salvar_em_csv(dados):
     df = pd.DataFrame(dados)
     df.to_csv('dados/relatorio.csv', index=False)
-    print("Relatório salvo com Sucesso!)
+    print("Relatório salvo com Sucesso!")
 
     #Execução Principal
     if __name__ == "__main__":
-        print('Iniciando ROBO...')
+        print("Iniciando ROBO...")
         dados = coletar_dados()
         salvar_em_csv(dados)
+
