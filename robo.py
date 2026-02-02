cb37bf17f010af0e86e38a114031de94096a72d0
Guilherme A. Santos <270210.gui@gmail.com>
2026-02-01 23:44:39 -0300

    Refactor data collection and saving functions

--- robo.py
+++ robo.py
@@ -1,22 +1,21 @@
 import datetime
 import pandas as pd
 
-#Simulação de coleta de dados
+# Simula coleta de dados
 def coletar_dados():
     return [
-        {"data": datetime.date.today(), "evento": "processamento finalizado","status": "OK"},
+        {"data": datetime.date.today(), "evento": "Processamento finalizado", "status": "OK"},
     ]
 
-#Salvar em um CSV
-def salvar_em_csv(dados):
+# Salva os dados em um CSV
+def salvar_relatorio(dados):
     df = pd.DataFrame(dados)
     df.to_csv('dados/relatorio.csv', index=False)
-    print("Relatório salvo com Sucesso!")
+    print("✅ Relatório salvo com sucesso!")
 
-    #Execução Principal
-    if __name__ == "__main__":
-        print("Iniciando ROBO...")
-        dados = coletar_dados()
-        salvar_em_csv(dados)
-
+# Execução principal
+if __name__ == "__main__":
+    print("🤖 Iniciando robô...")
+    dados = coletar_dados()
+    salvar_relatorio(dados)
 
